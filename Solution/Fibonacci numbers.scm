d8f05c2cb64ce288f5a5e2c880d64bd9183d2e4e
pi3rrot <4dakto@gmail.com>
2019-01-11 21:45:27 +0900

    fibonacci numbers

---
+++ Solution/Fibonacci numbers.scm
@@ -0,0 +1,29 @@
+; 0 1 1 2 3 5
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                  (fib (- n 2))))))
+
+
+;a ← a+b
+;b ← a
+(define (fib2 n) 
+  (fib-iter 1 0 n))
+
+(define (fib-iter a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+
+
+(fib2 5)
+
+(define (fib2 n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+        b
+        (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 0 n))
+      
