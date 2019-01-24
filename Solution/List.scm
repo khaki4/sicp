70101ac174101d1e9f429b67d0fb8c8555947e25
pi3rrot <4dakto@gmail.com>
2019-01-24 20:42:15 +0900

    map

--- Solution/List.scm
+++ Solution/List.scm
@@ -30,3 +30,28 @@
 (define odds (list 1 3 5 7))
 
 (length odds)
+
+;list mapping
+(define (scale-list items factor)
+  (if (null? items)
+    '()
+    (cons (* (car items) factor)
+      (scale-list (cdr items) factor))))
+
+(scale-list (list 1 2 3 4 5) 10)
+
+;map
+(define (map proc items)
+  (if (null? items)
+    '()
+    (cons (proc (car items))
+      (map proc (cdr items)))))
+
+(map abs (list -10 2.5 -11.6))
+
+;map by hoc
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor)) items))
+
+(scale-list (list 1 2 3 4 5) 10)
+
